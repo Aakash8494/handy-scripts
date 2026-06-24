1759d8f431c00a89e64d7d9944dff93a1aa35e09
Aakash8494 <72457949+Aakash8494@users.noreply.github.com>
2026-06-24 16:08:01 +0530

    chore : mac

--- batch_compress_lectures.sh
+++ batch_compress_lectures.sh
@@ -1,11 +1,11 @@
 mkdir -p compressed
 for f in *.mp4; do
   if [ ! -f "compressed/$f" ]; then
-    printf "\e[36mCompressing: %s...\e[0m\n" "$f"
+    printf '\e[36mCompressing: %s...\e[0m\n' "$f"
     ffmpeg -i "$f" -c:v libx265 -crf 28 -r 24 -preset fast -c:a aac -b:a 96k "compressed/$f"
-    printf "\e[32mFinished: %s\e[0m\n\n" "$f"
+    printf '\e[32mFinished: %s\e[0m\n\n' "$f"
   else
-    printf "\e[90mSkipping: %s (Already compressed)\e[0m\n" "$f"
+    printf '\e[90mSkipping: %s (Already compressed)\e[0m\n' "$f"
   fi
 done
-printf "\e[33mAll videos processed successfully!\e[0m\n"
+printf '\e[33mAll videos processed successfully!\e[0m\n'
